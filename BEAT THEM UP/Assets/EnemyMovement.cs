c1bf718456be9fb9fd897941d1a8fdb2f991d73c
yanis92bis <yanis171001@gmail.com>
2023-01-09 17:29:32 +0100

    script enemy

--- BEAT THEM UP/Assets/EnemyMovement.cs
+++ BEAT THEM UP/Assets/EnemyMovement.cs
@@ -4,15 +4,114 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+
+    [SerializeField] Animator animator;
+    [SerializeField] Rigidbody2D rb2D;
+    [SerializeField] EnemeState currentState;
+
+    CapsuleCollider2D cc2D;
+    float currentSpeed;
+
     // Start is called before the first frame update
+
+    public enum EnemeState
+    {
+        IDLE,
+        WALK,
+        ATTACK,
+        DEATH
+
+    }
+
     void Start()
     {
-        
+        rb2D = GetComponent<Rigidbody2D>();
+        cc2D = GetComponent<CapsuleCollider2D>();
+        currentState = EnemeState.IDLE;
+        OnStateEnter(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+     
+
+        OnStateUpdate();
     }
+
+    void OnStateEnter()
+    {
+        switch (currentState)
+        {
+            case EnemeState.IDLE:
+                break;
+            case EnemeState.WALK:
+                animator.SetBool("WALK", true);
+                break;
+            case EnemeState.ATTACK:
+                animator.SetTrigger("ATTACK");
+                break;
+            case EnemeState.DEATH:
+                break;
+            default:
+                break;
+        }
+
+    }
+
+
+    void OnStateUpdate()
+    {
+        switch (currentState)
+        {
+            case EnemeState.IDLE:
+                break;
+            case EnemeState.WALK:
+                break;
+            case EnemeState.ATTACK:
+                break;
+            case EnemeState.DEATH:
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    void OnStateExist()
+    {
+        switch (currentState)
+        {
+            case EnemeState.IDLE:
+                break;
+            case EnemeState.WALK:
+                animator.SetBool("WALK", false);
+                break;
+            case EnemeState.ATTACK:
+                animator.SetTrigger("ATTACK");
+                break;
+            case EnemeState.DEATH:
+                break;
+            default:
+                break;
+        }
+
+
+    }
+
+    void TransitionToState(EnemeState nextState)
+    {
+        OnStateExist();
+        currentState = nextState;
+        OnStateEnter();
+
+    }
+
+    void PlayerDetected()
+
+    {
+        Debug.Log("j'ai détécté le joueur");
+
+    }
+
 }
