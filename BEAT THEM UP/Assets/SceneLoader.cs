780a99e8504f0ce55056440f26bdecc1296b8356
yanis92bis <yanis171001@gmail.com>
2023-01-12 17:18:28 +0100

    scene loader in progresse

--- BEAT THEM UP/Assets/SceneLoader.cs
+++ BEAT THEM UP/Assets/SceneLoader.cs
@@ -2,33 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
-    ////METHODE 2
-    //public void ChargerUneScene(int BuildIndex)
-    //{
+    public GameObject loadingscreen;
+    public Slider slider;
 
-    //    SceneManager.LoadScene(BuildIndex);
-
-    //}
-
-    ////private void NextLevel()
-    ////{
-    ////    int index = SceneManager.GetActiveScene().buildIndex;
-    ////    SceneManager.LoadScene(index + 1);
-    ////}
-    //public void TryAgain()
-    //{
-    //    // INDEX DE LA SCENE ACTUELLE
-    //    int index = SceneManager.GetActiveScene().buildIndex;
-    //    // RECHARGE LA SCENE ACTUELLE
-    //    SceneManager.LoadScene(index);
-
-    //}
-
-
-
+    public void LoadLevel (int sceneindexe);
 
     public void ChargerLeJeu()
     {
