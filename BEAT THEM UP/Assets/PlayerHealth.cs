ae003f481ca131904b94166893aa5f6a385ee979
stantirou <stirou@icloud.com>
2023-01-17 10:59:07 +0100

    enemy script velocity

--- BEAT THEM UP/Assets/PlayerHealth.cs
+++ BEAT THEM UP/Assets/PlayerHealth.cs
@@ -1,23 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-
+    [SerializeField] GameObject slider;
     [SerializeField] float maxHealth = 100f;
     public float currentHealth;
-    
+
+    Slider healthSlider;
+
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        healthSlider = slider.GetComponent<Slider>();
+        healthSlider.maxValue = currentHealth;
+        healthSlider.value = maxHealth;
+
     }
 
     public void TakeDamage(float damageAmount)
     {
 
         currentHealth -= damageAmount;
+        healthSlider.value = currentHealth;
 
         if (currentHealth <= 0)
         {
